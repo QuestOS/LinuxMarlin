0ce037f9eda477edb35a1146b38694229bec5ec2
Tom Cheng <czq@bu.edu>
2016-03-10 18:00:03 -0500

    remove EEPROM_SETTING macro

--- ConfigurationStore.h
+++ ConfigurationStore.h
@@ -11,12 +11,7 @@ void Config_PrintSettings();
 static FORCE_INLINE void Config_PrintSettings() {}
 #endif
 
-#ifdef EEPROM_SETTINGS
-void Config_StoreSettings();
-void Config_RetrieveSettings();
-#else
 FORCE_INLINE void Config_StoreSettings() {}
 FORCE_INLINE void Config_RetrieveSettings() { Config_ResetDefault(); Config_PrintSettings(); }
-#endif
 
 #endif//CONFIG_STORE_H
